33cf53f1e84a9043df34c1e15864104e693391e9
“HiraGul” <hira24148@gmail.com>
2022-10-18 11:17:38 +0500

    Initial commit

---
+++ lib/Views/SharedComponents/demo.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DemoComponent extends StatelessWidget {
+  const DemoComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
